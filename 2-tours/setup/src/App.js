0293ca59c0fce3b2d4e83052f198a83af6338f2d
Pratham700 <prathamsinghal.singhal@gmail.com>
2020-11-24 13:54:17 +0530

    Changes

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -5,7 +5,58 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+const [loading,setLoad] = useState(true) ;
+const [tours,setTour] = useState([]) ;
+
+const removeTour = (id) => {
+  const newTour = tours.filter((tour) => tour.id !== id)
+  setTour(newTour) ;
+}
+
+const fetchTours = async () => {
+  setLoad(true) ;
+
+   try{
+    
+     const response = await fetch(url); 
+     const tours = await response.json() ;
+     setLoad(false) ;
+     setTour(tours) ;
+   }
+   catch(error) {
+    setLoad(false) ;
+    console.log(error)
+   }
+}
+
+useEffect( ()=> {
+  fetchTours()                    //Mounting Phase
+},[]) ;
+ 
+  if(loading == true) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if(tours.length == 0) {
+    return (
+      <main>
+        <div className = "title">
+          <h2>No More Tours For U</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour = {removeTour}/>
+    </main>
+  )
 }
 
 export default App

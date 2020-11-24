7ab2d090e06b3927a565e2508b12c4e18c836088
Pratham700 <prathamsinghal.singhal@gmail.com>
2020-11-24 18:08:45 +0530

    updated

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -3,7 +3,55 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index,setIndex] = useState(0) ;
+  const {name, job, image, text} = people[index] ;
+
+   const checKIndex =  (ind) => {
+     if(ind > people.length - 1){
+       return 0 ;
+     }
+     if(ind < 0 ){
+       return people.length-1 ;
+     }
+     return ind ;
+   } ;
+  const prevFunc = () => {
+    setIndex( (index) => {
+      let newIndex = index - 1 ;
+      return checKIndex(newIndex) ;
+    })
+  } ;
+  const nextFunc = () => {
+    setIndex( (index) => {
+      let newIndex = index + 1 ;
+      return checKIndex(newIndex) ;
+    })
+  } ;
+  
+  return (
+    <section className = "review">
+      <div className = "img-container">
+        <img className='person-img' src = {image} alt ={name}/>
+        <span className = 'quote-icon'>
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className = "author">{name}</h4>
+       <p className = "job" >{job}</p>
+       <p className = "info">{text}</p>
+        
+       <div className = "button-container">
+        <button className = "prev-btn" onClick={prevFunc}>
+          <FaChevronLeft/>
+        </button>
+        <button className='next-btn' onClick={nextFunc}>
+          <FaChevronRight />
+        </button>
+        
+       </div>
+      
+    </section>
+  );
 };
 
 export default Review;
